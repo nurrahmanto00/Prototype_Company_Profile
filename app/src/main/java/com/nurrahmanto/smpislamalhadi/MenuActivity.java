8229e4d89b9b3f60580aa8e640093ffca0f87db0
nurrahmanto00 <nurrahmanto00@gmail.com>
2022-03-22 23:53:41 +0700

    tambahan

--- app/src/main/java/com/nurrahmanto/smpislamalhadi/MenuActivity.java
+++ app/src/main/java/com/nurrahmanto/smpislamalhadi/MenuActivity.java
@@ -1,12 +1,11 @@
 package com.nurrahmanto.smpislamalhadi;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+
 
 public class MenuActivity extends Activity {
 
