3c6aff38a5f6dfed19b077698c3b8a24475f8858
nurrahmanto00 <nurrahmanto00@gmail.com>
2022-03-22 22:14:21 +0700

    Initial commit

---
+++ app/src/main/java/com/nurrahmanto/smpislamalhadi/MenuActivity.java
@@ -0,0 +1,34 @@
+package com.nurrahmanto.smpislamalhadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MenuActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(MenuActivity.this,MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+
+    }
+}
