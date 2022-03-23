ad06c29cf78c5b0a9db7b237fb3ef71fd3809ceb
nurrahmanto00 <nurrahmanto00@gmail.com>
2022-03-23 17:47:12 +0700

    tambahan

--- app/src/main/java/com/nurrahmanto/smpislamalhadi/MainActivity.java
+++ app/src/main/java/com/nurrahmanto/smpislamalhadi/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends Activity {
 
     ImageView gbr1;
     ImageView gbr2;
+    ImageView gbr3;
+    ImageView gbr4;
+    ImageView gbr5;
     ImageView gbr6;
 
     @Override
@@ -19,6 +22,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         gbr1 = findViewById(R.id.gbr1);
         gbr2 = findViewById(R.id.gbr2);
+        gbr3 = findViewById(R.id.gbr3);
+        gbr4 = findViewById(R.id.gbr4);
+        gbr5 = findViewById(R.id.gbr5);
         gbr6 = findViewById(R.id.gbr6);
 
 
@@ -38,6 +44,36 @@ public class MainActivity extends Activity {
             @Override
             public void onClick (View view) {
                 Intent intent = new Intent(MainActivity.this, GaleriActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        gbr3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View view) {
+                Intent intent = new Intent(MainActivity.this, ProfilActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        gbr4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View view) {
+                Intent intent = new Intent(MainActivity.this, ProfilActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        gbr5.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick (View view) {
+                Intent intent = new Intent(MainActivity.this, ProfilActivity.class);
                 startActivity(intent);
 
             }
